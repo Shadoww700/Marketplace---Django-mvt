44a1bcfe258cd3bdfd6c112c971c194578cefcb9
Shadow77 <shadowmaruf70@gmail.com>
2026-06-26 00:01:47 +0500

    Optimize shop views queries using select_related to completely eliminate N+1 problem

--- shop/views.py
+++ shop/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import Product, Category
 
 def shop_home(req):
-    products = Product.objects.all().order_by('-created_at')
+    products = Product.objects.all().select_related('category','seller' ).order_by('-created_at')
     categories = Category.objects.all()
+
+    if req.user.is_authenticated:
+        req.user = req.user.__class__.objects.select_related('profile').get(pk=req.user.pk)
 
     return render(req, 'shop/home.html', {
         'products': products,
