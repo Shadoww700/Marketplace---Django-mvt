7efbd4f4459a7ea79a1996c2e1b4e053ccfb5701
Shadow77 <shadowmaruf70@gmail.com>
2026-06-25 18:06:37 +0500

    feat(accounts): added CustomUser and EmailConfirm models with admin registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('id',)
+
+
+admin.register(models.EmailConfirm)
+class EmailConfirmAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'code')
