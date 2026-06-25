bc0ca88516975ef74b3ef31435b4dabb9d1a168d
Shadow77 <shadowmaruf70@gmail.com>
2026-06-25 23:47:27 +0500

    Create initial shop_home view with core novelty sorting query

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+def shop_home(req):
+    products = Product.objects.all().order_by('-created_at')
+    categories = Category.objects.all()
+
+    return render(req, 'shop/home.html', {
+        'products': products,
+        'categories': categories
+    })
