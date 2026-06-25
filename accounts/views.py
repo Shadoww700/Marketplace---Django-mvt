13905b4c8c436f4309585adcc0eb91760173b355
Shadow77 <shadowmaruf70@gmail.com>
2026-06-25 23:29:42 +0500

    Implement verify view with manual code matching, user activation, and shop profile initialization

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from . import models
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 import random
+from shop.models import Profile
 
 
 def register(req):
@@ -46,7 +48,38 @@ def register(req):
         print(f'Email confirmation send to : {user.email}')
         print(f'Code >>> {verifications_code} <<<')
         print('='*30 + '\n')
-        
-        return redirect('verify_email')
+
+
+        return redirect('verify_email', username=user.username)
 
     return render(req, 'accounts/register.html')
+
+
+def verify_email(req, username):
+    if req.method == 'POST':
+        input_code = req.POST.get('code', '').strip()
+
+        try:
+            user = models.CustomUser.objects.get(username=username)
+            confirm_record = models.EmailConfirm.objects.filter(user=user).first()
+
+            if not confirm_record:
+                return render(req, 'accounts/verify.html', {'error':'Verifications session not found'})
+            
+            if confirm_record.code == input_code:
+                user.is_active = True
+                user.save()
+
+                Profile.objects.get_or_create(user=user, role='CUSTOMER')
+
+                confirm_record.delete()
+
+                login(user, user)
+                
+                return redirect('shop_home')
+            else:
+                return render(req, 'accounts/verify.html', {'error':'Invalid code'})
+        except models.CustomUser.DoesNotExist:
+            return render(req, 'accounts/verify.html', {'error':'User not exists'})
+        
+    return render(req, 'accounts/verify.html')
